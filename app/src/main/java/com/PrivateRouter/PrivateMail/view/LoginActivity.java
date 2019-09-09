f97e9de33bf099a009aac11d3338ca7085339f42
Alexander Levitsky <alexander.v.levitsky@gmail.com>
2019-09-09 22:46:40 +0600

    Add work with host field in Login screen. Add HostManager class. Add save host URL in preferences.

--- app/src/main/java/com/PrivateRouter/PrivateMail/view/LoginActivity.java
+++ app/src/main/java/com/PrivateRouter/PrivateMail/view/LoginActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.webkit.URLUtil;
 import android.widget.EditText;
 
+import com.PrivateRouter.PrivateMail.BuildConfig;
 import com.PrivateRouter.PrivateMail.R;
 import com.PrivateRouter.PrivateMail.model.Account;
 import com.PrivateRouter.PrivateMail.model.FolderType;
 import com.PrivateRouter.PrivateMail.model.errors.ErrorType;
 import com.PrivateRouter.PrivateMail.network.logics.LoginLogic;
+import com.PrivateRouter.PrivateMail.repository.HostManager;
 import com.PrivateRouter.PrivateMail.repository.LoggedUserRepository;
 import com.PrivateRouter.PrivateMail.view.mail_list.MailListActivity;
 import com.PrivateRouter.PrivateMail.view.utils.RequestViewUtils;
@@ -19,6 +22,7 @@ import com.PrivateRouter.PrivateMail.view.utils.RequestViewUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.fabric.sdk.android.services.network.UrlUtils;
 
 public class LoginActivity extends AppCompatActivity implements LoginLogic.OnLoginCallback {
 
@@ -37,9 +41,13 @@ public class LoginActivity extends AppCompatActivity implements LoginLogic.OnLog
     public void btLoginClick() {
         String login = etEmail.getText().toString();
         String pass = etPassword.getText().toString();
+        String host = etHost.getText().toString();
 
-        if(checkFieldsDataCorrect()) {
+        if (checkFieldsDataCorrect()) {
             RequestViewUtils.showRequest(this);
+
+            HostManager.setHost(host);
+
             LoginLogic loginLogic = new LoginLogic(this);
             loginLogic.login(login, pass, this);
         }
@@ -55,8 +63,11 @@ public class LoginActivity extends AppCompatActivity implements LoginLogic.OnLog
         Window window = getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
+        etHost.setText(HostManager.getHost());
+
         checkLogged();
     }
+
 
     private void checkLogged() {
         boolean logged = LoggedUserRepository.getInstance().load(this);
@@ -72,7 +83,7 @@ public class LoginActivity extends AppCompatActivity implements LoginLogic.OnLog
 
         Account account = LoggedUserRepository.getInstance().getActiveAccount();
         String folder = account.getFolders().getFolderName(FolderType.Inbox);
-        
+
         Intent intent = MailListActivity.makeIntent(this, folder);
         startActivity(intent);
         finish();
@@ -84,19 +95,39 @@ public class LoginActivity extends AppCompatActivity implements LoginLogic.OnLog
         RequestViewUtils.showError(this, errorType, serverCode);
     }
 
-    private boolean checkFieldsDataCorrect(){
-        if(etEmail.getText().toString().isEmpty()){
+    private boolean checkFieldsDataCorrect() {
+        String host = etHost.getText().toString();
+
+
+        if (etEmail.getText().toString().isEmpty()) {
             etEmail.setError(getString(R.string.all_email_is_empty));
             etEmail.requestFocus();
             return false;
 
-        }
-        else if(!EmailValidator.isValidEmail(etEmail.getText().toString())){
+        } else if (!EmailValidator.isValidEmail(etEmail.getText().toString())) {
             etEmail.setError(getString(R.string.all_email_is_incorrect));
             etEmail.requestFocus();
             return false;
-        } else{
-            return true;
+
+        } else if (host.isEmpty()) {
+            etHost.setError(getString(R.string.all_host_is_empty));
+            etHost.requestFocus();
+            return false;
+
+        }  else if (!URLUtil.isValidUrl(host)) {
+            etHost.setError(getString(R.string.all_host_is_incorrect));
+            etHost.requestFocus();
+            return false;
         }
+
+
+        char hostLastCharacter = host.charAt(host.length() - 1);
+
+        if (!Character.toString(hostLastCharacter).equals("/")) {
+            StringBuilder stringBuilder = new StringBuilder(host);
+            stringBuilder.append("/");
+            etHost.setText(stringBuilder);
+        }
+        return true;
     }
 }
